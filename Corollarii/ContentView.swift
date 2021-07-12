62c32fbfd074ee22099783be9d511eaf4a965861
OberstKrueger <ok@oberstkrueger.com>
2021-07-11 19:47:47 -0700

    Brittle first implementation

--- Corollarii/ContentView.swift
+++ Corollarii/ContentView.swift
@@ -1,9 +1,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var manager = CorollariiManager()
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section(header: Text("Enter Bill Amount")) {
+                    TextField("Bill Amount", text: $manager.amount)
+                        .keyboardType(.decimalPad)
+                }
+                Section {
+                    Stepper("Tip Percent: \(manager.percentage)", value: $manager.percentage)
+                    Toggle("Round Up", isOn: $manager.round)
+                }
+                Section {
+                    Text("Tip Amount: $\(manager.calculateTip().description)")
+                    Text("Total: $\(manager.calculateTotal().description)")
+                }
+            }
+            .navigationTitle("Corollarii")
+        }
     }
 }
 
--- Corollarii/CorollariiManager.swift
+++ Corollarii/CorollariiManager.swift
@@ -1,6 +1,76 @@
+import Foundation
 import SwiftUI
 
 /// Manager object.
 class CorollariiManager: ObservableObject {
-    
+    // MARK: - Internal Properties
+    fileprivate var calculatedTip: Decimal {
+        if var calculated = Decimal(string: amount) {
+            var rounded = Decimal()
+
+            calculated *= (Decimal(percentage) / 100)
+            NSDecimalRound(&rounded, &calculated, 2, .bankers)
+
+            return rounded
+        } else {
+            return .nan
+        }
+    }
+
+    fileprivate var calculatedTotal: Decimal {
+        if let calculated = Decimal(string: amount) {
+            return calculated + calculatedTip
+        } else {
+            return .nan
+        }
+    }
+
+    // MARK: - Public Properties
+    @Published var amount: String = "0" {
+        didSet {
+            switch amount.count {
+            case 0:
+                amount = "0"
+            case 1:
+                break
+            default:
+                if amount.first == "0" {
+                    amount = String(amount.dropFirst())
+                }
+            }
+
+            
+        }
+    }
+
+    @Published var round: Bool = false
+
+    @Published var percentage: UInt = 15
+
+    // MARK: - Public Methods
+    func calculateTip() -> Decimal {
+        if round {
+            var rounded = Decimal()
+            var tempTotal = calculatedTotal
+
+            NSDecimalRound(&rounded, &tempTotal, 0, .up)
+
+            return calculatedTip + rounded - calculatedTotal
+        } else {
+            return calculatedTip
+        }
+
+    }
+
+    func calculateTotal() -> Decimal {
+        if round {
+            if let calculated = Decimal(string: amount) {
+                return calculated + calculateTip()
+            } else {
+                return .nan
+            }
+        } else {
+            return calculatedTotal
+        }
+    }
 }
